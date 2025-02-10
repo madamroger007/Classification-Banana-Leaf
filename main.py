467f07c66e330f7d171469fd56963c8a8bcce221
Madamroger <104338817+madamroger007@users.noreply.github.com>
2025-02-10 17:48:28 +0700

    update main.py
    debug = false

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ def get_output():
 
 if __name__ =='__main__':
 	#app.debug = True
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
